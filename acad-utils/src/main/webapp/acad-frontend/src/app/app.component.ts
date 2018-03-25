62db685d8838fb51ef04570ed694822c629d4c4e
Oleh <skyba.oleg96@gmail.com>
2018-03-25 18:07:06 +0300

    restyled sidenav and main content, added back link for wildcard router component

--- acad-utils/src/main/webapp/acad-frontend/src/app/app.component.ts
+++ acad-utils/src/main/webapp/acad-frontend/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  events = [];
+  routes = [
+    { label: 'Home', path: '/home' },
+    { label: 'Schema view', path: '/schema/1' },
+  ];
 }
